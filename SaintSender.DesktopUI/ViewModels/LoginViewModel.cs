96638272d313dad30b94b7f7aaaea6e57c6ebb01
Gergő Ádám <rapture047@gmail.com>
2019-07-02 14:56:56 +0200

    connection to imap works

---
+++ SaintSender.DesktopUI/ViewModels/LoginViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SaintSender.Core.Services;
+
+namespace SaintSender.DesktopUI.ViewModels
+{
+    public class LoginViewModel
+    {
+        AuthenticationService authenticationService = new AuthenticationService();
+        public void Sign_In(string username, string password)
+        {
+            authenticationService.ConnectToIMAPService(username, password);
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+        }
+    }
+}
